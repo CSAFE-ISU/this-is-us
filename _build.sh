379786657159c6d557834f11c14aa4ce21e1d35e
Susan Vanderplas <srvanderplas@gmail.com>
2019-10-31 13:43:35 -0500

    Automatically run gitignore and git vaccinate every time the thing is compiled.

--- _build.sh
+++ _build.sh
@@ -2,7 +2,9 @@
 
 set -ev
 
+Rscript -e "usethis::git_vaccinate()"
+Rscript -e "usethis::use_git_ignore(ignores=c('.gitignore', '.directory', 'bookdown-demo*', 'docs-local', '_bookdown_files/'))"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
